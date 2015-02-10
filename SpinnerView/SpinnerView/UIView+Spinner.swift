1b06fc006ba0abc3a7cbe250f30defd7e8a3fe9f
Valentin Leonov <Valentin.Leonov@noveogroup.com>
2015-02-10 15:43:32 +0600

    + skeleton for spinner view

---
+++ SpinnerView/SpinnerView/UIView+Spinner.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIView {
+    
+    func startSpinner() {
+        let spinnerView = UIView(frame: self.bounds)
+        spinnerView
+    }
+}
--- SpinnerView/SpinnerView/ViewController.swift
+++ SpinnerView/SpinnerView/ViewController.swift
@@ -3,21 +3,24 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var vExperimental: UIView!
-    @IBOutlet weak var lcBottom: NSLayoutConstraint!
-    @IBOutlet weak var lcLeft: NSLayoutConstraint!
-    @IBOutlet weak var lcTop: NSLayoutConstraint!
-    @IBOutlet weak var lcRight: NSLayoutConstraint!
+    @IBOutlet weak var lcWidth: NSLayoutConstraint!
+    @IBOutlet weak var lcHeight: NSLayoutConstraint!
     
     
     @IBAction func btnResizeClicked(sender: UIButton) {
         
-        let maxWidth = self.view.bounds.size.width - self.lcLeft.constant - self.lcRight.constant
-        let width = Float(arc4random_uniform(UInt32(maxWidth)))
+        let maxWidth = self.view.bounds.size.width
+        let width = arc4random_uniform(UInt32(maxWidth))
+        self.lcWidth.constant = CGFloat(width)
         
-        let maxHeight = self.view.bounds.size.height - self.lcBottom.constant - self.lcTop.constant
-        let height = Float(arc4random_uniform(UInt32(maxHeight)))
+        let maxHeight = self.view.bounds.size.height
+        let height = arc4random_uniform(UInt32(maxHeight))
+        self.lcHeight.constant = CGFloat(height)
         
-        println("mW = \(maxWidth), mH = \(maxHeight), w = \(width), h = \(height)")
-        self.vExperimental.frame.size = CGSizeMake(CGFloat(width), CGFloat(height))
+        self.view.setNeedsLayout()
+    }
+    
+    @IBAction func btnStartSpinnerClicked(sender: UIButton) {
+        self.vExperimental.startSpinner()
     }
 }
